326b27f1b342e09624882f67037c84c36dafc412
Reema <reema_alsamrani@hotmail.com>
2026-05-09 22:34:18 +0300

    Update config.php

--- saudi-pro/saudi_project/config.php
+++ saudi-pro/saudi_project/config.php
@@ -1,7 +1,7 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "saudi_db", 3306);
-
-if ($conn->connect_error) {
+try{
+    $conn = mysqli_connect("127.0.0.1", "root", "", "saudi_db", 3306);
+}catch(Exception $e){
     die("فشل الاتصال بقاعدة البيانات");
 }
 ?>
